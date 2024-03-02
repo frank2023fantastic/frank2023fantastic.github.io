a630e8294836c2ae1a09cae64ae6bc40d2ec022b
frank2lele <157940640+frank2023fantastic@users.noreply.github.com>
2024-03-02 22:24:41 +0800

    add

--- scripts.js
+++ scripts.js
@@ -1,12 +1,2 @@
 // JavaScript code for your blog website (if any)
 // You can add your dynamic behavior here
-
-import { createApp, ref } from 'vue'
-
-createApp({
-  setup() {
-    return {
-      count: ref(0)
-    }
-  }
-}).mount('#app')
